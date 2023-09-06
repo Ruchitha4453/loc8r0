abd343fba8de8741f3b1787da75c73e1fd05c22d
rlc08 <21eg107a44@anurag.edu.in>
2023-09-06 10:54:49 +0530

    final

---
+++ app_server/model/db.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+let dbURI="mongodb://0.0.0.0:27017/";
+if (process.env.NODE_ENV === 'production') {
+  dbURI = process.env.MONGODB_URI;
+}
+mongoose.connect(dbURI);
+
+
+mongoose.connection.on('connected', () => {
+  console.log(`Mongoose connected`);
+});
+mongoose.connection.on('error', err => {
+  console.log('Mongoose connection error:', err);
+});
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose disconnected');
+});
+
+const gracefulShutdown = (msg, callback) => {
+  mongoose.connection.close( () => {
+    console.log(`Mongoose disconnected through ${msg}`);
+    callback();
+  });
+};
+
+
+require('./location');
+
